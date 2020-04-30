ffcf0e0f0ecd5750f60be0ddb81de90a86cbce0f
Andres Gonzalez <Andres Gonzalez@DESKTOP-JDM2H35>
2020-04-30 00:05:41 -0600

    Creación de la clase QueueException

---
+++ src/creational/QueueException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package creational;
+
+/**
+ *
+ * @author Andres Gonzalez
+ */
+public class QueueException extends Exception {
+
+    public QueueException(String message) {
+        super(message);
+    }
+    
+    
+}
