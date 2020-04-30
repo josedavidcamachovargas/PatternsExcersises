01a0fb63d23ce2b1a6ffc438113eef42eda48906
Andres Gonzalez <Andres Gonzalez@DESKTOP-JDM2H35>
2020-04-30 00:03:31 -0600

    Creacion de la clase Queue

---
+++ src/creational/Queue.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package creational;
+
+/**
+ *
+ * @author Andres Gonzalez
+ */
+public class Queue {
+    private Node front;
+    private Node rear;
+    private int maxItems, totalItems;
+
+    public Queue(int size) {
+        maxItems = size;
+    }
+    
+    //Enqueue method
+    //Dequeue method
+    //peek method
+    //isEmpty method
+}
