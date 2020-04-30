659da831ae42996a6f7111e37886f0826871095d
Andres Gonzalez <Andres Gonzalez@DESKTOP-JDM2H35>
2020-04-30 00:12:50 -0600

    Creación de los metodos enqueue y dequeue de la clase Queue

--- src/creational/Queue.java
+++ src/creational/Queue.java
@@ -10,6 +10,7 @@ package creational;
  * @author Andres Gonzalez
  */
 public class Queue {
+
     private Node front;
     private Node rear;
     private int maxItems, totalItems;
@@ -17,9 +18,36 @@ public class Queue {
     public Queue(int size) {
         maxItems = size;
     }
-    
+
     //Enqueue method
+    public int enqueue(int num) throws QueueException {
+        Node n = new Node();
+        n.setNum(num);
+        if (maxItems > 0 && totalItems == maxItems) {
+            throw new QueueException("La cola esta llena");
+        } else if (front == null) {
+            front = rear = n;
+        } else {
+            totalItems++;
+        }
+        return rear.getNum();
+    }
+
     //Dequeue method
+    public int dequeue(int num) throws QueueException {
+        if (front == null) {
+            throw new QueueException("La cola esta vacía");
+        }
+        int newNum;
+        newNum = front.getNum();
+        front = front.getNext();
+        if (front == null) {
+            rear = null;
+        }
+        totalItems++;
+        return newNum;
+    }
     //peek method
     //isEmpty method
+
 }
