28f09d4eacdcb13b45af5e97731141f40ddf8352
Andres Gonzalez <Andres Gonzalez@DESKTOP-JDM2H35>
2020-04-29 23:52:01 -0600

    Clase Nodo con metodos set y get

--- src/creational/Node.java
+++ src/creational/Node.java
@@ -10,5 +10,26 @@ package creational;
  * @author Andres Gonzalez
  */
 public class Node {
+    private int num;
+    private Node next;
+
+    public Node() {
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
     
 }
