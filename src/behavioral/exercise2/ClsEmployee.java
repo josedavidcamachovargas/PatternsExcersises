77e43b0d6b0a426f744e3d5f990589e4f9aa8f86
Andres Gonzalez <Andres Gonzalez@DESKTOP-JDM2H35>
2020-05-02 17:01:28 -0600

    Creacion de la clase Employee

---
+++ src/behavioral/exercise2/ClsEmployee.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package behavioral.exercise2;
+
+/**
+ *
+ * @author Andres Gonzalez
+ */
+public class ClsEmployee {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
